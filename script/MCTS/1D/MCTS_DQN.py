6417375911305f05d7147c0c328942470aa6d6c6
wenyuhan <wh1264@nyu.edu>
2021-08-03 17:58:27 -0400

    modify 1D MCTS and run some experiments

--- script/MCTS/1D/MCTS_DQN.py
+++ script/MCTS/1D/MCTS_DQN.py
@@ -5,7 +5,6 @@ Created on Sun Jun 27 18:19:21 2021
 
 @author: hanwenyu
 """
-
 import torch
 import torch.nn as nn
 import numpy as np
@@ -16,33 +15,37 @@ from collections import deque
 import random
 import sys
 sys.path.append('../../../Env/1D/')
-from DMP_Env_1D_static_MCTS_obs import deep_mobile_printing_1d1r_MCTS_obs
+sys.path.append('../utils')
+from DMP_Env_1D_static_MCTS_obs import deep_mobile_printing_1d1r_MCTS
 import uct
-
-
-log_path="./log/DQN_1d_Step_lr0.01_rollouts50_ucb_constant0.5/"
-
-env = deep_mobile_printing_1d1r_MCTS_obs(plan_choose=2)
-
-if os.path.exists(log_path)==False:
-    os.makedirs(log_path)
+from tensorboardX import SummaryWriter
 
 ## hyper parameter
 minibatch_size=2000
-Lr=0.01
+Lr=0.0001
 N_iteration=3000
 N_iteration_test=3
 alpha=0.9
 Replay_memory_size=50000
 Update_traget_period=200
-Action_dim=env.action_dim
-State_dim=env.state_dim
 UPDATE_FREQ=1
 INITIAL_EPSILON = 0.1
 FINAL_EPSILON = 0.0
+PALN_CHOICE=2  ##0: sin 1: Guassian 2:step
+ROLLOUT=20
+UCB_CONSTANT=2
+device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+PLAN_LIST=["sin","Gaussian","step"]
+PLAN_NAME=PLAN_LIST[PALN_CHOICE]
+OUT_FILE_NAME="DQN_1d_"+PLAN_NAME+"_lr"+str(Lr)+"_rollouts"+str(ROLLOUT)+"_ucb_constant"+str(UCB_CONSTANT)
+print(OUT_FILE_NAME)
+log_path="./log/static/"+OUT_FILE_NAME+"/"
+env = deep_mobile_printing_1d1r_MCTS(plan_choose=PALN_CHOICE)
 
-
-
+if os.path.exists(log_path)==False:
+    os.makedirs(log_path)
+Action_dim=env.action_dim
+State_dim=env.state_dim
 print("state_dim",State_dim)
 print("total_step",env.total_step)
 
@@ -74,9 +77,9 @@ class Q_NET(nn.Module):
 
 UCT_mcts = uct.UCT(
     action_space=env.action_space,
-    rollouts=50,
+    rollouts=ROLLOUT,
     horizon=100,
-    ucb_constant=0.5,
+    ucb_constant=UCB_CONSTANT,
     is_model_dynamic=True
 )
 
@@ -98,8 +101,7 @@ class DQN_AGNET():
         # index=self.count_memory%Replay_memory_size
         self.replay_memory.append((s,a,r,s_next))
         self.count_memory+=1
-    def choose_action(self,env,state,obs,done):
-        
+    def choose_action(self,env,state,obs,done):        
         action=UCT_mcts.act(env,self.Eval_net,state,obs,done,self.device)   
         return action
     def learning_process(self):
@@ -107,39 +109,25 @@ class DQN_AGNET():
         self.Eval_net.train()
         if self.learn_step% Update_traget_period == 0:
             self.Target_net.load_state_dict(self.Eval_net.state_dict())
-            
         minibatch = random.sample(self.replay_memory,minibatch_size)
-
-
         current_states = []
         acts = []
         rewards = []
-        next_states = []
-       
+        next_states = []      
         for b in minibatch:
             current_states.append(b[0])
             acts.append(b[1])
             rewards.append(b[2])
-            next_states.append(b[3])
-            
-        current_states = np.array(current_states)
-        
-        acts = np.array(acts).reshape(minibatch_size,1)
-        
+            next_states.append(b[3])           
+        current_states = np.array(current_states)       
+        acts = np.array(acts).reshape(minibatch_size,1)       
         rewards = np.array(rewards).reshape(minibatch_size,1)
-        next_states = np.array(next_states)
-        
-        
+        next_states = np.array(next_states)       
         b_state = torch.from_numpy(current_states).float().to(device).squeeze(1)
         b_action = torch.from_numpy(acts).float().to(device)
-        b_reward = torch.from_numpy(rewards).float().to(device)
-        
+        b_reward = torch.from_numpy(rewards).float().to(device)        
         b_state_next = torch.from_numpy(next_states).float().to(device).squeeze(1)
-        
-
         Q_eval = self.Eval_net(b_state,b_action)
-        
-
         Q=torch.zeros(minibatch_size,Action_dim).to(device)
         for item in range(Action_dim):
             a=torch.FloatTensor(np.array([item])).unsqueeze(0).to(device)
@@ -147,16 +135,14 @@ class DQN_AGNET():
             Q_value=self.Target_net(b_state_next,a).detach().squeeze(1)
             Q[:,item]=Q_value 
         Q_next=torch.max(Q,dim=1)[0].reshape(minibatch_size,1)
-        Q_target = b_reward + alpha * Q_next
-        
-        loss = self.loss(Q_eval, Q_target)
-        
+        Q_target = b_reward + alpha * Q_next        
+        loss = self.loss(Q_eval, Q_target)        
         loss.backward()
         self.optimizer.step()
         self.learn_step+=1
-        self.loss_his.append(loss.item())
-
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+        train_loss=loss.item()
+        return train_loss 
+        
 # device = torch.device("cpu")
 agent=DQN_AGNET(device)
 number_steps=0
@@ -168,7 +154,6 @@ while True:
         agent.store_memory(prev_obs, action, reward, next_obs)
         prev_obs = next_obs
         number_steps+=1
-
         if done:
             break
         if number_steps==Replay_memory_size:
@@ -176,34 +161,23 @@ while True:
     if number_steps==Replay_memory_size:
             break
 
-
-
 agent.greedy_epsilon=INITIAL_EPSILON
-
 best_reward=0
 total_steps = 0
-
-reward_history_train=[]
-reward_history_test=[]
-iou_history_train=[]
-iou_history_test=[]
+writer = SummaryWriter('./DQN_1d_static')
 for episode in range(N_iteration):
-    state, obs = env.reset()
-    
+    state, obs = env.reset()   
     reward_train = 0
     start_time = time.time()
     done=False
     while True:
         total_steps +=1
         action = agent.choose_action(env,state,obs,done)
-        state_next,obs_next, r, done = env.step(action)
-  
-       
+        state_next,obs_next, r, done = env.step(action)       
         agent.store_memory(obs, action, r, obs_next)
         reward_train += r
         if total_steps % UPDATE_FREQ == 0:
-            agent.learning_process()
-            
+            train_loss=agent.learning_process()            
         if done:
             secs = int(time.time() - start_time)
             mins = secs / 60
@@ -211,16 +185,11 @@ for episode in range(N_iteration):
             print(" | time in %d minutes, %d seconds\n" %(mins, secs))
             print('Epodise: ', episode,
                   '| Ep_reward_train: ', reward_train)
-            reward_history_train.append(reward_train)
         if done:
-
             break
         state = state_next
-        obs = obs_next
-       
+        obs = obs_next       
     train_iou=env.iou()
-    iou_history_train.append(train_iou)
-
     iou_test=0
     reward_test_total=0
     IOU_test_total=0
@@ -229,16 +198,11 @@ for episode in range(N_iteration):
         state, obs = env.reset()
         reward_test=0
         done=False
-        while True:
-      
+        while True:     
             action = agent.choose_action(env,state,obs,done)
-            
-
             state_next,obs_next, r, done = env.step(action)
-            reward_test += r
-            
+            reward_test += r           
             if done:
-
                 break
             state = state_next
             obs = obs_next
@@ -254,12 +218,6 @@ for episode in range(N_iteration):
           '| Ep_reward_test:', reward_test_total)
     print('\nEpodise: ', episode,
           '| Ep_IOU_test: ', iou_test/N_iteration_test)
-
-    reward_history_test.append(reward_test_total)
-    iou_history_test.append(IOU_test_total)
-
-
-
     if IOU_test_total >= best_reward:
         torch.save(agent.Eval_net.state_dict(), log_path+'Eval_net_episode_%d.pth' % (episode))
         torch.save(agent.Target_net.state_dict(), log_path+'Target_net_episode_%d.pth' % (episode))
@@ -269,24 +227,14 @@ for episode in range(N_iteration):
         with open(log_path+"position.pickle", "wb") as fp: 
             pickle.dump(env.position_memory, fp)
         best_reward=IOU_test_total
-
-
-
-with open(log_path+"brick_last.pickle", "wb") as fp: 
-    pickle.dump(env.brick_memory, fp)
-with open(log_path+"position_last.pickle", "wb") as fp: 
-    pickle.dump(env.position_memory, fp)
-        
-
-
-with open(log_path+"reward_his_train.pickle", "wb") as fp: 
-    pickle.dump(reward_history_train, fp)
-with open(log_path+"reward_his_test.pickle", "wb") as fp: 
-    pickle.dump(reward_history_test, fp)
-with open(log_path+"loss.pickle", "wb") as fp: 
-    pickle.dump(agent.loss_his, fp)  
-
-with open(log_path+"iou_train_history.pickle", "wb") as fp: 
-    pickle.dump(iou_history_train, fp) 
-with open(log_path+"iou_test_history.pickle", "wb") as fp: 
-    pickle.dump(iou_history_test, fp)  
+    writer.add_scalars(OUT_FILE_NAME,
+                                   {'train_loss': train_loss,
+                                    'train_reward': reward_train,
+                                    'train_iou': train_iou,
+                                   'test_reward': reward_test_total,
+                                   'test_iou':IOU_test_total,}, episode)
+JSON_log_PATH="./JSON/"
+if os.path.exists(JSON_log_PATH)==False:
+    os.makedirs(JSON_log_PATH)                                   
+writer.export_scalars_to_json(JSON_log_PATH+OUT_FILE_NAME+".json")
+writer.close()
--- script/MCTS/1D/MCTS_DQN_dynamic.py
+++ script/MCTS/1D/MCTS_DQN_dynamic.py
@@ -16,16 +16,10 @@ from collections import deque
 import random
 import sys
 sys.path.append('../../../Env/1D/')
+sys.path.append('../utils/')
 from DMP_Env_1D_dynamic_MCTS_obs import deep_mobile_printing_1d1r_MCTS_obs
 import uct_dynamic
-
-
-log_path="./log/Dynamic/DQN_1d_dynamic_lr0.0001_rollouts10_ucb_constant6.364/"
-
-env = deep_mobile_printing_1d1r_MCTS_obs()
-
-if os.path.exists(log_path)==False:
-    os.makedirs(log_path)
+from tensorboardX import SummaryWriter
 
 ## hyper parameter
 minibatch_size=2000
@@ -35,14 +29,23 @@ N_iteration_test=3
 alpha=0.9
 Replay_memory_size=50000
 Update_traget_period=200
-Action_dim=env.action_dim
-State_dim=env.state_dim
 UPDATE_FREQ=1
 INITIAL_EPSILON = 0.1
 FINAL_EPSILON = 0.0
+ROLLOUT=20
+UCB_CONSTANT=2
+INITIAL_EPSILON = 0.1
+FINAL_EPSILON = 0.0
+device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+OUT_FILE_NAME="DQN_1d_sincurve_lr"+str(Lr)+"_rollouts"+str(ROLLOUT)+"_ucb_constant"+str(UCB_CONSTANT)
+print(OUT_FILE_NAME)
+log_path="./log/dynamic/"+OUT_FILE_NAME+"/"
+env = deep_mobile_printing_1d1r_MCTS_obs()
+if os.path.exists(log_path)==False:
+    os.makedirs(log_path)
+Action_dim=env.action_dim
+State_dim=env.state_dim
 plan_dim=env.plan_width
-
-
 print("state_dim",State_dim)
 print("total_step",env.total_step)
 
@@ -74,9 +77,9 @@ class Q_NET(nn.Module):
 
 UCT_mcts = uct_dynamic.UCT(
     action_space=env.action_space,
-    rollouts=10,
+    rollouts=ROLLOUT,
     horizon=100,
-    ucb_constant=6.364,
+    ucb_constant=UCB_CONSTANT,
     is_model_dynamic=True
 )
 
@@ -95,46 +98,38 @@ class DQN_AGNET():
     def store_memory(self,s,a,r,s_next,env_plan):
         self.replay_memory.append((s,a,r,s_next,env_plan))
         self.count_memory+=1
-    def choose_action(self,env,state,obs,done):
-        
+    def choose_action(self,env,state,obs,done):       
         action=UCT_mcts.act(env,self.Eval_net,state,obs,done,self.device)   
         return action
     def learning_process(self):
         self.optimizer.zero_grad()
         self.Eval_net.train()
         if self.learn_step% Update_traget_period == 0:
-            self.Target_net.load_state_dict(self.Eval_net.state_dict())
-            
+            self.Target_net.load_state_dict(self.Eval_net.state_dict())           
         minibatch = random.sample(self.replay_memory,minibatch_size)
-
-
         current_states = []
         acts = []
         rewards = []
         next_states = []
-        env_plans = []
-       
+        env_plans = []       
         for b in minibatch:
             current_states.append(b[0])
             acts.append(b[1])
             rewards.append(b[2])
             next_states.append(b[3])
-            env_plans.append(b[4])
-            
+            env_plans.append(b[4])           
         current_states = np.array(current_states)
         # print("current_states_shape",current_states.shape)
         acts = np.array(acts).reshape(minibatch_size,1)
         # print("action_shape",acts.shape)
         rewards = np.array(rewards).reshape(minibatch_size,1)
         next_states = np.array(next_states)
-        env_plans = np.array(env_plans)
-        
+        env_plans = np.array(env_plans)        
         b_state = torch.from_numpy(current_states).float().to(device).squeeze(1)
         b_action = torch.from_numpy(acts).float().to(device)
         b_reward = torch.from_numpy(rewards).float().to(device)
         b_state_next = torch.from_numpy(next_states).float().to(device).squeeze(1)
-        b_env_plans = torch.from_numpy(env_plans).float().to(device)
-        
+        b_env_plans = torch.from_numpy(env_plans).float().to(device)        
         Q_eval = self.Eval_net(b_state,b_action,b_env_plans)
         Q=torch.zeros(minibatch_size,Action_dim).to(device)
         for item in range(Action_dim):
@@ -144,15 +139,13 @@ class DQN_AGNET():
             Q[:,item]=Q_value 
         Q_next=torch.max(Q,dim=1)[0].reshape(minibatch_size,1)
         Q_target = b_reward + alpha * Q_next
-        loss = self.loss(Q_eval, Q_target)
-        
+        loss = self.loss(Q_eval, Q_target)       
         loss.backward()
         self.optimizer.step()
         self.learn_step+=1
-        self.loss_his.append(loss.item())
+        train_loss=loss.item()
+        return train_loss 
 
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-# device = torch.device("cpu")
 agent=DQN_AGNET(device)
 number_steps=0
 while True:
@@ -163,7 +156,6 @@ while True:
         agent.store_memory(prev_obs, action, reward, next_obs,env.plan)
         prev_obs = next_obs
         number_steps+=1
-
         if done:
             break
         if number_steps==Replay_memory_size:
@@ -171,20 +163,12 @@ while True:
     if number_steps==Replay_memory_size:
             break
 
-
-
 agent.greedy_epsilon=INITIAL_EPSILON
-
 best_reward=0
 total_steps = 0
-
-reward_history_train=[]
-reward_history_test=[]
-iou_history_train=[]
-iou_history_test=[]
+writer = SummaryWriter('./DQN_1d_dynamic')
 for episode in range(N_iteration):
-    state, obs = env.reset()
-    
+    state, obs = env.reset()   
     reward_train = 0
     start_time = time.time()
     done=False
@@ -192,13 +176,10 @@ for episode in range(N_iteration):
         total_steps +=1
         action = agent.choose_action(env,state,obs,done)
         state_next,obs_next, r, done = env.step(action)
-  
-       
         agent.store_memory(obs, action, r, obs_next,env.plan)
         reward_train += r
         if total_steps % UPDATE_FREQ == 0:
-            agent.learning_process()
-            
+            train_loss=agent.learning_process()           
         if done:
             secs = int(time.time() - start_time)
             mins = secs / 60
@@ -206,16 +187,11 @@ for episode in range(N_iteration):
             print(" | time in %d minutes, %d seconds\n" %(mins, secs))
             print('Epodise: ', episode,
                   '| Ep_reward_train: ', reward_train)
-            reward_history_train.append(reward_train)
         if done:
-
             break
         state = state_next
-        obs = obs_next
-       
+        obs = obs_next       
     train_iou=env.iou()
-    iou_history_train.append(train_iou)
-
     iou_test=0
     reward_test_total=0
     IOU_test_total=0
@@ -225,15 +201,10 @@ for episode in range(N_iteration):
         reward_test=0
         done=False
         while True:
-      
             action = agent.choose_action(env,state,obs,done)
-            
-
             state_next,obs_next, r, done = env.step(action)
             reward_test += r
-            
             if done:
-
                 break
             state = state_next
             obs = obs_next
@@ -250,39 +221,24 @@ for episode in range(N_iteration):
     print('\nEpodise: ', episode,
           '| Ep_IOU_test: ', iou_test/N_iteration_test)
     print("\nDQN_1d_dynamic_lr0.0001_rollouts10_ucb_constant6.364")
-
-    reward_history_test.append(reward_test_total)
-    iou_history_test.append(IOU_test_total)
-
-
-
     if IOU_test_total >= best_reward:
         torch.save(agent.Eval_net.state_dict(), log_path+'Eval_net_episode_%d.pth' % (episode))
         torch.save(agent.Target_net.state_dict(), log_path+'Target_net_episode_%d.pth' % (episode))
-
         with open(log_path+"brick.pickle", "wb") as fp: 
             pickle.dump(env.brick_memory, fp)
         with open(log_path+"position.pickle", "wb") as fp: 
             pickle.dump(env.position_memory, fp)
         best_reward=IOU_test_total
+    writer.add_scalars(OUT_FILE_NAME,
+                                   {'train_loss': train_loss,
+                                    'train_reward': reward_train,
+                                    'train_iou': train_iou,
+                                   'test_reward': reward_test_total,
+                                   'test_iou':IOU_test_total,}, episode)
+JSON_log_PATH="./JSON/"
+if os.path.exists(JSON_log_PATH)==False:
+    os.makedirs(JSON_log_PATH)                                   
+writer.export_scalars_to_json(JSON_log_PATH+OUT_FILE_NAME+".json")
+writer.close()
 
 
-
-with open(log_path+"brick_last.pickle", "wb") as fp: 
-    pickle.dump(env.brick_memory, fp)
-with open(log_path+"position_last.pickle", "wb") as fp: 
-    pickle.dump(env.position_memory, fp)
-        
-
-
-with open(log_path+"reward_his_train.pickle", "wb") as fp: 
-    pickle.dump(reward_history_train, fp)
-with open(log_path+"reward_his_test.pickle", "wb") as fp: 
-    pickle.dump(reward_history_test, fp)
-with open(log_path+"loss.pickle", "wb") as fp: 
-    pickle.dump(agent.loss_his, fp)  
-
-with open(log_path+"iou_train_history.pickle", "wb") as fp: 
-    pickle.dump(iou_history_train, fp) 
-with open(log_path+"iou_test_history.pickle", "wb") as fp: 
-    pickle.dump(iou_history_test, fp)  
