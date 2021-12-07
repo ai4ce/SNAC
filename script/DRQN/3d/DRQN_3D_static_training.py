7bcd396607db292f400822e782a29edfe139d802
wenyuhan <wh1264@nyu.edu>
2021-12-07 12:45:03 -0500

    modify DRQN hindsight 1D static

--- script/DRQN/3d/DRQN_3D_static_training.py
+++ script/DRQN/3d/DRQN_3D_static_training.py
@@ -7,23 +7,22 @@ import random
 import time
 import os
 from collections import deque
-
-sys.path.append('../../Env/3D/')
+from tensorboardX import SummaryWriter
+sys.path.append('../../../Env/3D/')
 from DMP_simulator_3d_static_circle import deep_mobile_printing_3d1r
 
-# plan_choose: 0 Dense circle, 1 Sparse circle
-plan_choose = 1
-log_path = "./log/DRQN/3D/Static/plan_"+str(plan_choose)+"/"
-if os.path.exists(log_path) == False:
-    os.makedirs(log_path)
-print('3D_Static')
-print('plan_choose:',plan_choose)
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-env = deep_mobile_printing_3d1r(plan_choose=plan_choose)
-print("device_using:", device)
-
+def set_seed(seeds):
+    torch.manual_seed(seeds)
+    torch.cuda.manual_seed_all(seeds)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seeds)
+    random.seed(seeds)
+    os.environ['PYTHONHASHSEED'] = str(seeds)
 ######################
 # hyper parameter
+seeds=5
+set_seed(seeds)
 minibatch_size=64
 Lr=0.00001
 N_iteration=10000
@@ -31,40 +30,42 @@ N_iteration_test=10
 alpha=0.9
 Replay_memory_size=1000
 Update_traget_period=200
-Action_dim=env.action_dim
-State_dim=env.state_dim
 hidden_state_dim=256
 Time_step=20
 UPDATE_FREQ=5
 INITIAL_EPSILON = 0.2
 FINAL_EPSILON = 0.0
 ######################
-
-
+# plan_choose: 0 Dense circle, 1 Sparse circle
+PALN_CHOICE=0  # 0 dense 1 sparse
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+PLAN_LIST=["dense","sparse"]
+PLAN_NAME=PLAN_LIST[PALN_CHOICE]
+OUT_FILE_NAME="DRQN_3d_"+PLAN_NAME+"_lr"+str(Lr)+"_seed_"+str(seeds)
+print(OUT_FILE_NAME)
+log_path="/mnt/NAS/home/WenyuHan/SNAC/DRQN/3D/log/static/"+OUT_FILE_NAME+"/"
+env = deep_mobile_printing_3d1r(plan_choose=PALN_CHOICE)
+print("device_using:", device)
+Action_dim=env.action_dim
+State_dim=env.state_dim
 print("State_dim:",State_dim)
 print("plan_width:", env.plan_width)
 print("^^^^^^^^^^^^^^^^^^^^^^^^^^")
-
-
-
+if os.path.exists(log_path) == False:
+    os.makedirs(log_path)
 
 def get_and_init_FC_layer(din, dout):
     li = nn.Linear(din, dout)
-    li.weight.data.normal_(0, 0.1)
+    nn.init.xavier_uniform_(
+       li.weight.data, gain=nn.init.calculate_gain('relu'))
+    li.bias.data.fill_(0.)
     return li
-
-
 
 class Q_NET(nn.Module):
     def __init__(self,out_size,hidden_size):
         super(Q_NET, self).__init__()
-        
         self.out_size = out_size
         self.hidden_size = hidden_size
-
-       
-
-
         self.fc_1 = get_and_init_FC_layer(State_dim, 64)
         self.fc_2 = get_and_init_FC_layer(64, 128)
         self.fc_3 = get_and_init_FC_layer(128, 128)
@@ -73,19 +74,13 @@ class Q_NET(nn.Module):
         self.val  = get_and_init_FC_layer(hidden_size, 1)
         self.relu = nn.ReLU()
     def forward(self,x,bsize,time_step,hidden_state,cell_state):
-        
-        
-
         x=x.view(bsize*time_step,State_dim)
-        
-        
         x = self.fc_1(x)
         x = self.relu(x)
         x = self.fc_2(x)
         x = self.relu(x)
         x = self.fc_3(x)
         x = self.relu(x)
-        
         x = x.view(bsize,time_step,128)
         lstm_out = self.rnn(x,(hidden_state,cell_state))
         out = lstm_out[0][:,time_step-1,:]
@@ -100,17 +95,12 @@ class Q_NET(nn.Module):
         c = torch.zeros(1,bsize,self.hidden_size).float().to(device)
         return h,c
 
-
-
 class Memory():
-    
     def __init__(self,memsize):
         self.memsize = memsize
         self.memory = deque(maxlen=self.memsize)
-    
     def add_episode(self,epsiode):
         self.memory.append(epsiode)
-    
     def get_batch(self,bsize,time_step):
         sampled_epsiodes = random.sample(self.memory,bsize)
         batch = []
@@ -118,7 +108,6 @@ class Memory():
             point = np.random.randint(0,len(episode)+1-time_step)
             batch.append(episode[point:point+time_step])
         return batch
-
 
 class DQN_AGNET():
     def __init__(self,device):
@@ -133,12 +122,8 @@ class DQN_AGNET():
         self.greedy_epsilon=0.2
         self.replaymemory=Memory(Replay_memory_size)
     def choose_action(self,s,hidden_state,cell_state):
-        
         state=torch.from_numpy(s).float().to(self.device)
-        
-       
         choose=np.random.uniform()
-       
         if choose<=self.greedy_epsilon:
             model_out = self.Eval_net.forward(state,bsize=1,time_step=1,hidden_state=hidden_state,cell_state=cell_state)
             action=np.random.randint(0, Action_dim)
@@ -153,20 +138,16 @@ class DQN_AGNET():
             
         return action, hidden_state, cell_state
     def learning_process(self):
-        
         self.optimizer.zero_grad()
         self.Eval_net.train()
         if self.learn_step% Update_traget_period == 0:
             self.Target_net.load_state_dict(self.Eval_net.state_dict())
-
         hidden_batch, cell_batch = self.Eval_net.init_hidden_states(bsize=minibatch_size)
         batch = self.replaymemory.get_batch(bsize=minibatch_size,time_step=Time_step)  
-
         current_states = []
         acts = []
         rewards = []
         next_states = []
-       
         for b in batch:
             cs,ac,rw,ns = [],[],[],[]
             for element in b:
@@ -174,54 +155,35 @@ class DQN_AGNET():
                 ac.append(element[1])
                 rw.append(element[2])
                 ns.append(element[3])
-                
             current_states.append(cs)
             acts.append(ac)
             rewards.append(rw)
-            next_states.append(ns)
-            
+            next_states.append(ns)  
         current_states = np.array(current_states)
         acts = np.array(acts)
         rewards = np.array(rewards)
         next_states = np.array(next_states)
-        
-        
         torch_current_states = torch.from_numpy(current_states).float().to(self.device)
         torch_acts = torch.from_numpy(acts).long().to(self.device)
         torch_rewards = torch.from_numpy(rewards).float().to(self.device)
         torch_next_states = torch.from_numpy(next_states).float().to(self.device)
-       
-        
-
         Q_s, _ = self.Eval_net.forward(torch_current_states,bsize=minibatch_size,time_step=Time_step,hidden_state=hidden_batch,cell_state=cell_batch)
-        
         Q_s_a = Q_s.gather(dim=1,index=torch_acts[:,Time_step-1].unsqueeze(dim=1)).squeeze(dim=1)
-        
-
         Q_next,_ = self.Target_net.forward(torch_next_states,bsize=minibatch_size,time_step=Time_step,hidden_state=hidden_batch,cell_state=cell_batch)
         Q_next_max,__ = Q_next.detach().max(dim=1)
         target_values = torch_rewards[:,Time_step-1] + (alpha * Q_next_max)
-       
-
-        
         loss = self.loss(Q_s_a, target_values)
-        
         loss.backward()
         self.optimizer.step()
         self.learn_step+=1
-        self.loss_his.append(loss.item())
-
-
-
+        train_loss=loss.item()
+        return train_loss
 #### initial fill the replaymemory 
-
-
 # device = torch.device("cpu")
 agent=DQN_AGNET(device)
 while len(agent.replaymemory.memory)<Replay_memory_size:
     prev_state = env.reset()
     local_memory = []
-    
     while True:
         action = np.random.randint(0,Action_dim)
         next_state,reward,done = env.step(action)
@@ -231,29 +193,19 @@ while len(agent.replaymemory.memory)<Replay_memory_size:
             break
     if len(local_memory)>=Time_step:
         agent.replaymemory.add_episode(local_memory)
-
-
-
 agent.greedy_epsilon=INITIAL_EPSILON
-
 print("agent greedy_epsilon", agent.greedy_epsilon)
-
 best_reward=-500
 total_steps = 0
-reward_history_train=[]
-reward_history_test=[]
-iou_history_train=[]
-iou_history_test=[]
+writer = SummaryWriter('/mnt/NAS/home/WenyuHan/SNAC/DRQN/3D/DRQN_3d_static')
 
 for episode in range(N_iteration):
     state = env.reset()
-    
     print("total_brick",env.total_brick)
     reward_train = 0
     start_time = time.time()
     local_memory=[]
     hidden_state, cell_state = agent.Eval_net.init_hidden_states(bsize=1)
-
     while True:
         total_steps +=1
         action,hidden_state_next, cell_state_next = agent.choose_action(state,hidden_state, cell_state)
@@ -261,18 +213,14 @@ for episode in range(N_iteration):
         local_memory.append((state, action, r, state_next))
         reward_train += r
         if total_steps % UPDATE_FREQ == 0:
-            agent.learning_process()
-            
+            train_loss=agent.learning_process()
         if done:
-            reward_history_train.append(reward_train)
             break
         state = state_next
         hidden_state, cell_state = hidden_state_next, cell_state_next
     if len(local_memory)>=Time_step:
         agent.replaymemory.add_episode(local_memory)
-    iou_train=env.iou()
-    iou_history_train.append(iou_train)
-
+    train_iou=env.iou()
   ############ test agent 
     iou_test=0
     reward_test_total=0
@@ -281,54 +229,42 @@ for episode in range(N_iteration):
         state = env.reset()
         env_plan=env.input_plan
         reward_test=0
-
         hidden_state, cell_state = agent.Eval_net.init_hidden_states(bsize=1)
-
         while True:
-
             action, hidden_state_next, cell_state_next = agent.choose_action(state, hidden_state, cell_state)
-
             state_next, r, done = env.step(action)
-
             reward_test += r
-
             if done:
                 break
             state = state_next
             hidden_state, cell_state = hidden_state_next, cell_state_next
-
         reward_test_total += reward_test
         iou_test += env.iou()
     reward_test_total = reward_test_total / N_iteration_test
+    IOU_test_total= iou_test/N_iteration_test
     secs = int(time.time() - start_time)
     mins = secs / 60
     secs = secs % 60
     print('Epodise: ', episode,
-          '| Ep_reward_test:', reward_test_total, '| Ep_IOU_test: ', iou_test / N_iteration_test)
+          '| Ep_reward_test:', reward_test_total, '| Ep_IOU_test: ', IOU_test_total)
     print(" | time in %d minutes, %d seconds\n" % (mins, secs))
-
-    reward_history_test.append(reward_test_total)
-    iou_history_test.append(iou_test / N_iteration_test)
-
     if agent.greedy_epsilon > FINAL_EPSILON:
         agent.greedy_epsilon -= (INITIAL_EPSILON - FINAL_EPSILON)/N_iteration
-
     if reward_test_total > best_reward:
         torch.save(agent.Eval_net.state_dict(), log_path+'Eval_net_episode_%d.pth' % (episode))
         torch.save(agent.Target_net.state_dict(), log_path+'Target_net_episode_%d.pth' % (episode))
         best_reward=reward_test_total
-
-with open(log_path+"reward_his_train.pickle", "wb") as fp: 
-    pickle.dump(reward_history_train, fp)
-with open(log_path+"reward_his_test.pickle", "wb") as fp: 
-    pickle.dump(reward_history_test, fp)
-with open(log_path+"loss.pickle", "wb") as fp: 
-    pickle.dump(agent.loss_his, fp)  
-
-with open(log_path+"iou_train_history.pickle", "wb") as fp: 
-    pickle.dump(iou_history_train, fp) 
-with open(log_path+"iou_test_history.pickle", "wb") as fp: 
-    pickle.dump(iou_history_test, fp)   
+    writer.add_scalars(OUT_FILE_NAME,
+                                   {'train_loss': train_loss,
+                                    'train_reward': reward_train,
+                                    'train_iou': train_iou,
+                                   'test_reward': reward_test_total,
+                                   'test_iou':IOU_test_total,}, episode)
+JSON_log_PATH="./JSON/"
+if os.path.exists(JSON_log_PATH)==False:
+    os.makedirs(JSON_log_PATH)                                   
+writer.export_scalars_to_json(JSON_log_PATH+OUT_FILE_NAME+".json")
+writer.close()
 
 
 
