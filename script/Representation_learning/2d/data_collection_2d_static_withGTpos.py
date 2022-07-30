9426995af8c3ade1263e6528d0b14fdea1a8557b
WenyuHan-LiNa <wh1264@nyu.edu>
2022-07-30 01:19:15 -0400

    add explicit lstm

--- script/Representation_learning/2d/data_collection_2d_static_withGTpos.py
+++ script/Representation_learning/2d/data_collection_2d_static_withGTpos.py
@@ -1,28 +1,28 @@
 import sys
 sys.path.append('../../../Env/2D/')
-from DMP_Env_2D_static_normalized import deep_mobile_printing_2d1r
+from DMP_Env_2D_static_normalized_GTpos import deep_mobile_printing_2d1r
 import joblib
 import os
 import numpy as np
 
 replay_memory = []
-env = deep_mobile_printing_2d1r(plan_choose=0)
+env = deep_mobile_printing_2d1r(plan_choose=1)
 
-for episode in range(100):
+for episode in range(30000):
     print(episode)
     obs = env.reset()
     local_memory = []
     while True:
         action = np.random.randint(5)
         new_obs, reward, done = env.step(action)
-        local_memory.append((obs))
+        local_memory.append((obs,action,new_obs))
         obs = new_obs
         if done:
             break
     replay_memory.append(local_memory)
 
-# save_path = "/mnt/NAS/home/WenyuHan/SNAC/Representation/2D/"
-save_path = "./"
+save_path = "/mnt/NAS/home/WenyuHan/SNAC/Representation/2D/"
+# save_path = "./"
 if os.path.exists(save_path) == False:
     os.makedirs(save_path)
-joblib.dump(replay_memory, save_path+"data_2d_static_sparse_normalized_30000.pkl")
+joblib.dump(replay_memory, save_path+"data_2d_static_sparse_normalized_GTpos_30000.pkl")
--- script/Representation_learning/2d/model.py
+++ script/Representation_learning/2d/model.py
@@ -1,6 +1,13 @@
 import torch 
 import torch.nn as nn
 
+def get_and_init_FC_layer(din, dout):
+    li = nn.Linear(din, dout)
+    nn.init.xavier_uniform_(
+       li.weight.data, gain=nn.init.calculate_gain('relu'))
+    li.bias.data.fill_(0.)
+    return li
+
 class RecurrentEncoder(nn.Module):
     """Recurrent encoder"""
     def __init__(self, input_size, hidden_size,device):
@@ -22,7 +29,7 @@ class RecurrentDecoder(nn.Module):
         self.output_size = output_size
         self.device = device
         self.rec_dec1 = nn.GRUCell(output_size, hidden_size)
-        self.dense_dec1 = nn.Linear(hidden_size, output_size)
+        self.dense_dec1 = get_and_init_FC_layer(hidden_size, output_size)
     def forward(self, h_0, seq_len):
         # Initialize output
         x = torch.tensor([], device = self.device)
@@ -40,7 +47,10 @@ class RecurrentDecoder(nn.Module):
         return x.view(-1, seq_len, self.output_size)
 
 class RecurrentAE(nn.Module):
-    """Recurrent autoencoder"""
+    """Recurrent autoencoder
+       input: a sequence of obs with size (B,L,51) and hidden state
+       output: a sequence of obs with size (B,L,49*3+2)
+    """
     def __init__(self,input_size, output_size, hidden_size, device, train=True):
         super().__init__()
         # Encoder and decoder configuration
@@ -63,3 +73,67 @@ class RecurrentAE(nn.Module):
         h_n = self.encoder(x,hidden_state)
         out = self.decoder(h_n, seq_len)
         return torch.flip(out, [1])
+
+class SNAC_Lnet(nn.Module):
+    """Recurrent Lnet
+       input: a sequence of obs with size (B,L,51) and hidden state
+       output: a sequence of obs with size (B,L,49*3+2)
+    """
+    def __init__(self,input_size, hidden_size, device, Loss_type="L2"):
+        super().__init__()
+        # Encoder and decoder configuration
+        self.hidden_size = hidden_size
+        self.input_size = input_size
+        if Loss_type == "L2": 
+            self.output_size = 2
+        else:
+            self.output_size = 26*26 
+        self.device = device
+        self.Loss_type = Loss_type
+        # Encoder and decoder
+        self.rnn = nn.LSTM(self.input_size, self.hidden_size,batch_first=True).to(device)
+        if Loss_type == "L2":
+            self.MLP = nn.Sequential(
+                        get_and_init_FC_layer(self.hidden_size,64),
+                        nn.ReLU(),
+                        get_and_init_FC_layer(64,16),
+                        nn.ReLU(),
+                        get_and_init_FC_layer(16,self.output_size),
+                        nn.ReLU())
+        else: 
+            self.MLP = nn.Sequential(
+                        get_and_init_FC_layer(self.hidden_size,256),
+                        nn.ReLU(),
+                        get_and_init_FC_layer(256,512),
+                        nn.ReLU(),
+                        get_and_init_FC_layer(512,self.output_size),
+                        nn.LogSoftmax(dim=2))
+
+    def forward(self, x, pos, hidden_state, cell_state):
+        """
+        x: size (B,L,K), K = 51+51+1 (two obs + action)
+        pos: size (B,L,2)
+        """
+        seq_len = x.shape[1]
+        B_size = x.shape[0]
+        predicted_pos = []
+        input_pos = pos[:,0:1,:]
+        for i in range(0, seq_len):
+            output, (hidden_state,cell_state) = self.rnn(torch.cat((x[:,i:i+1,:],input_pos),dim=2),(hidden_state,cell_state)) ### output shape = (B,1,hidden size)
+            if self.Loss_type == "L2":
+                next_pos = self.MLP(output) ## next pos size (B,1,2)
+                predicted_pos.append(next_pos)
+                input_pos = next_pos
+            else:
+                next_pos = self.MLP(output) ## next pos size (B,1,576), 26**2
+                next_pos = next_pos.view(B_size,1,26,26)
+                predicted_pos.append(next_pos)
+                if i >= (seq_len-1):
+                    break
+                input_pos = pos[:,i+1:i+2,:]
+        return predicted_pos, hidden_state, cell_state
+        
+    def init_hidden_states(self,bsize):
+        h = torch.zeros(1,bsize,self.hidden_size).float().to(self.device)
+        c = torch.zeros(1,bsize,self.hidden_size).float().to(self.device)
+        return h,c
